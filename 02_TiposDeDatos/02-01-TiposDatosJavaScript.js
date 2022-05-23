01ffce375991115fc6fa9dbca8dca393bd2c933f
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-23 09:13:17 -0500

    Tipos de datos

---
+++ 02_TiposDeDatos/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,15 @@
+// TODO: tipo de dato STRING
+var nombre = "Cristina";
+console.log(nombre);
+
+// TODO: Tipo e dato NUMERICO
+var numero = 1000;
+console.log(numero);
+
+// TODO: Tipo de dato OBJECT
+var Object = {
+  nombre: "Teddy",
+  apellido: "Summers",
+  telefono: "1234567890",
+};
+console.log(Object)
