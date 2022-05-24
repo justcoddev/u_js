92c24e83ef14ca5de834e7c4c8a4853aa7231e7e
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-23 23:35:15 -0500

    let - Const - 23/05/2022

---
+++ 02_TiposDeDatos/var_let_const.js
@@ -0,0 +1,8 @@
+// TODO: Variables que posteriormente podemos cambiar su valor
+let nombre;
+nombre = "Teddy";
+console.log(nombre);
+
+// TODO: variables que ya no queremos que se modifique su valor
+const apellido = 'Kirk';
+apellido = 'Lra';
