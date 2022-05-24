fdcb05a7f696ca7a255bf641810a8e7f8d8ab856
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-23 23:46:49 -0500

    23/05/2022

---
+++ 02_TiposDeDatos/BuenasPracticas.js
@@ -0,0 +1,8 @@
+let nombreCompleto = 'Teddy Summers';
+console.log(nombreCompleto);
+
+let x, y;
+x = 10, y = 20;
+
+let z = x+y;
+console.log(z);
---
+++ 02_TiposDeDatos/ReglasDefinir.js
@@ -0,0 +1,10 @@
+let nombreCompleto = "Teddy summers";
+let nombrecompleto = "Pool summers";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+// TODO: let 1nombreCompleto; no está permitido iniciar el nombre de uan variable con numero.
