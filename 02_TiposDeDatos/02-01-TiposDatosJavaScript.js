3a2d8b8a3498d779d97e21f56450cf3af10c7a28
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-23 21:44:57 -0500

    Tipos de Datos 23/05/2022

--- 02_TiposDeDatos/02-01-TiposDatosJavaScript.js
+++ 02_TiposDeDatos/02-01-TiposDatosJavaScript.js
@@ -13,3 +13,46 @@ var Object = {
   telefono: "1234567890",
 };
 console.log(Object)
+
+// TODO: Tipo de dato boolean(true, false)
+var bandera = false;
+console.log(bandera);
+
+// TODO: tipo de dato function
+function miFuncion(){}
+console.log(miFuncion);
+
+// TODO: Tipo de dato Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+// TODO:  Tipo clase es una function
+class Persona{
+  constructor(nombre, apellido){
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+}
+
+console.log(Persona);
+
+// TODO: Tipo undefined
+var x;
+console.log(x);
+
+x= undefined
+console.log(x)
+
+// TODO: null = ausencia de valor (es un tipo object)
+var y = null;
+console.log(y);
+
+
+// TODO: ARREGLOS son Object
+var autos=['BMW','Audi', 'Volvo']
+console.log(autos);
+console.log(typeof autos);
+
+var z = '';
+console.log(z);
+console.log(typeof z);
