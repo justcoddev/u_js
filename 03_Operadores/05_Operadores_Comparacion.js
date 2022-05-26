4fe66d90ce5f3ff0dcb8f15545eb251b4720c0e6
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-25 23:35:45 -0500

    Operador comparacion 25/05/2022

---
+++ 03_Operadores/05_Operadores_Comparacion.js
@@ -0,0 +1,10 @@
+let a = 3,
+  b = 2,
+  c = "3";
+
+let z = a == b; // TODO: revisa el valor sin importar el tipo
+console.log(z);
+
+z = a === c; // TODO:  revisa los valores pero tambien los tipos
+console.log(z);
+
