2a82ce0480bff725dc9f2843427edc1cf5422a74
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-25 23:44:03 -0500

    Operador Distinto a ... 25/05/2022

---
+++ 03_Operadores/06_Operador_Distinto_a.js
@@ -0,0 +1,9 @@
+let a = 3,
+  b = 2,
+  c = "3";
+
+let z = a != c; // TODO: revisa el valor sin importar el tipo
+console.log(z);
+
+z = a !== c; // TODO:  revisa los valores pero tambien los tipos
+console.log(z);
