bd29b43e5eeceddb2e98bab39730187f9b3eade0
justcoddev <edisonpaulcrz@gmail.com>
2022-09-14 02:37:21 -0500

    14/09/2022

---
+++ 003_Operadores/12_Operador_Ternario.js
@@ -0,0 +1,6 @@
+let resultado = (3 > 2) ? "verdadero" : "falso";
+console.log(resultado)
+
+let numero = 9;
+resultado = (numero % 2 == 0) ? "Número par" : "Número impar";
+console.log(resultado)
---
+++ 003_Operadores/13_String_Numero.js
@@ -0,0 +1,17 @@
+let miNumero = "10";
+
+// console.log(typeof miNumero)
+
+let edad = Number(miNumero);
+// console.log(typeof edad)
+
+if (edad >= 18) {
+  console.log("Puede votar")
+
+} else {
+  console.log("Muy joven para votar")
+  
+}
+
+let resultado = (edad >= 18) ? "Puede votar" : "Muy joven para votar"
+console.log(resultado)
