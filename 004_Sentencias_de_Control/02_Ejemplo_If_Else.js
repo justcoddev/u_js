67a4fe0f3c6ad2ae0f98b8562b5985027dec7cd1
justcoddev <edisonpaulcrz@gmail.com>
2022-09-14 10:27:24 -0500

    Sentencia If / Else ejemplo

---
+++ 004_Sentencias_de_Control/02_Ejemplo_If_Else.js
@@ -0,0 +1,18 @@
+let numero = 0;
+
+if (numero == 1) {
+  console.log("condición uno");
+}
+else if (numero == 2) {
+  console.log("condición dos");
+
+}
+else if (numero == 3) {
+  console.log("condición dos");
+
+}
+else if (numero == 4) {
+  console.log("condición dos");
+}else{
+  console.log("Número desconocido");
+}
